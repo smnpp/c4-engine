1a163ff7d9380302f60979817ca649701e32cf29
smnpp <simonperret69@gmail.com>
2025-01-15 11:59:52 +0100

    prédicats manquant

--- connect4.pl
+++ connect4.pl
@@ -123,19 +123,93 @@ initialize :-
                 ]
             )).
 
-play(Player) :-
+goodbye :-
     board(B),
-    output_board(B),
-    (
-        win(B, 'x') -> write('Player X wins!'), nl;
-        win(B, 'o') -> write('Player O wins!'), nl;
-        is_draw(B) -> write('It\'s a draw!'), nl;
-        make_move(Player, B, B2),
-        next_player(Player, NextPlayer),
-        retract(board(_)),
-        asserta(board(B2)),
-        play(NextPlayer)
-    )
+    nl,
+    nl,
+    write('Game over: '),
+    output_winner(B),
+    retract(board(_)),
+    retract(player(_,_)),
+    read_play_again(V), !,
+    (V == 'Y' ; V == 'y'), 
+    !,
+    run
+    .
+
+read_play_again(V) :-
+    nl,
+    nl,
+    write('Play again (Y/N)? '),
+    read(V),
+    (V == 'y' ; V == 'Y' ; V == 'n' ; V == 'N'), !
+    .
+
+read_play_again(V) :-
+    nl,
+    nl,
+    write('Please enter Y or N.'),
+    read_play_again(V)
+    .
+
+
+read_players :-
+    nl,
+    nl,
+    write('Number of human players? '),
+    read(N),
+    set_players(N)
+    .
+
+set_players(0) :- 
+    asserta( player(1, computer) ),
+    asserta( player(2, computer) ), !
+    .
+
+set_players(1) :-
+    nl,
+    write('Is human playing X or O (X moves first)? '),
+    read(M),
+    human_playing(M), !
+    .
+
+set_players(2) :- 
+    asserta( player(1, human) ),
+    asserta( player(2, human) ), !
+    .
+
+set_players(N) :-
+    nl,
+    write('Please enter 0, 1, or 2.'),
+    read_players
+    .
+
+
+human_playing(M) :- 
+    (M == 'x' ; M == 'X'),
+    asserta( player(1, human) ),
+    asserta( player(2, computer) ), !
+    .
+
+human_playing(M) :- 
+    (M == 'o' ; M == 'O'),
+    asserta( player(1, computer) ),
+    asserta( player(2, human) ), !
+    .
+
+human_playing(M) :-
+    nl,
+    write('Please enter X or O.'),
+    set_players(1)
+    .
+
+play(P) :-
+    board(B), !,
+    output_board(B), !,
+    not(game_over(P, B)), !,
+    make_move(P, B), !,
+    next_player(P, P2), !,
+    play(P2), !
     .
 
 
